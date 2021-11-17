6775a3dae1dae48797fadc41971f29c633aff0a0
Alessio Vinci <alessio.leonardo.vinci@hs-furtwangen.de>
2021-11-17 13:50:34 +0100

    finished morph vertex manipulation

--- sprint1_threeJS_Engine/src/rtcg-app/RTCG.js
+++ sprint1_threeJS_Engine/src/rtcg-app/RTCG.js
@@ -22,10 +22,30 @@ let endPos = {}
 let mouseDown = false;
 let material;
 let Time = 0;
-let q = new Vector3(0.25, 0.25, 0.25);
-let u = new Vector3(-0.5, 0, 0);
-let v = new Vector3(0, -0.5, 0);
-let cube;
+let sideLength = 1;
+let cubeArray =
+    [new Vector3(sideLength / 2, sideLength / 2, sideLength / 2),
+    new Vector3(-sideLength / 2, -sideLength / 2, -sideLength / 2),
+    new Vector3(-sideLength, 0, 0),
+    new Vector3(0, -sideLength, 0),
+    new Vector3(-sideLength, 0, 0),
+    new Vector3(0, 0, -sideLength),
+    new Vector3(0, -sideLength, 0),
+    new Vector3(0, 0, -sideLength),
+    new Vector3(sideLength, 0, 0),
+    new Vector3(0, sideLength, 0),
+    new Vector3(sideLength, 0, 0),
+    new Vector3(0, 0, sideLength),
+    new Vector3(0, sideLength, 0),
+    new Vector3(0, 0, sideLength)];
+let morph;
+
+function inRange(v, min, max) {
+    if (v >= min && v <= max) {
+        return true;
+    }
+    return false;
+}
 
 function planeIntersect(p1, p2, q, u, v) {
     let a = new Vector3(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
@@ -34,10 +54,36 @@ function planeIntersect(p1, p2, q, u, v) {
     m.set(a.x, u.x, v.x, a.y, u.y, v.y, a.z, u.z, v.z);
     m.invert();
     b.applyMatrix3(m);
-    return b;
+    if (inRange(b.x, 0, 1) && inRange(b.y, 0, 1) && inRange(b.z, 0, 1)) {
+        let intersection = new Vector3(p1.x + b.x * (p2.x - p1.x), p1.y + b.x * (p2.y - p1.y), p1.z + b.x * (p2.z - p1.z) );
+        return {valid: true, result: b, intersection, distance: p1.distanceTo(intersection)};
+    }
+    return {valid: false};
 }
-function sMax(a,b,k) {
-    return ((a+b)+Math.sqrt((a-b)*(a-b)+k))/2;
+
+function distToCube(p1, p2) {
+    let dist = Infinity;
+    for (let i = 0; i < 3; i++) {
+        let a = cubeArray[i * 2 + 2];
+        let b = cubeArray[i * 2 + 1 + 2];
+        let res = planeIntersect(p1, p2, cubeArray[0], a, b);
+        if(res.valid) {
+            dist = Math.min(dist, res.distance);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        let a = cubeArray[i * 2 + 8];
+        let b = cubeArray[i * 2 + 1 + 8];
+        let res = planeIntersect(p1, p2, cubeArray[1], a, b);
+        if(res.valid) {
+            dist = Math.min(dist, res.distance);
+        }
+    }
+    return dist;
+}
+
+function sMax(a, b, k) {
+    return ((a + b) + Math.sqrt((a - b) * (a - b) + k)) / 2;
 }
 
 class RTCG {
@@ -60,21 +106,22 @@ class RTCG {
 
         });
 
-        cube = createCube(material);
-        scene.add(cube);
+        morph = createCube(material);
+        //morph = createSphere(material);
+        scene.add(morph);
 
-        animator = new Animator(this.render, cube);
-        animator.add(cube);
+        animator = new Animator(this.render, morph);
+        animator.add(morph);
         animator.add(camera);
 
         toolSwitcher = new ToolSwitcher(animator, renderer, camera);
-        toolSwitcher.turnOnMouseAnimationAppending(cube);
-        /*animator.addTimeRestrainedAnimation(cube, "move", {x: 3.5}, 2, false, 0);
-        animator.addTimeRestrainedAnimation(cube, "move", {y: 3.5}, 2, false, 0);
-        animator.addTimeRestrainedAnimation(cube, "move", {x: -7}, 2, false, 0);
-        animator.addTimeRestrainedAnimation(cube, "move", {y: -7}, 2, false, 0);
-        animator.addTimeRestrainedAnimation(cube, "move", {z: 10}, 1, false, 1);*/
-        animator.addContinuousAnimation(cube, "rotate", { x: 1, y: 1 });
+        toolSwitcher.turnOnMouseAnimationAppending(morph);
+        /*animator.addTimeRestrainedAnimation(morph, "move", {x: 3.5}, 2, false, 0);
+        animator.addTimeRestrainedAnimation(morph, "move", {y: 3.5}, 2, false, 0);
+        animator.addTimeRestrainedAnimation(morph, "move", {x: -7}, 2, false, 0);
+        animator.addTimeRestrainedAnimation(morph, "move", {y: -7}, 2, false, 0);
+        animator.addTimeRestrainedAnimation(morph, "move", {z: 10}, 1, false, 1);*/
+        animator.addContinuousAnimation(morph, "rotate", { x: 1, y: 1 });
         animator.start();
 
     }
@@ -87,45 +134,22 @@ class RTCG {
 
         material.uniforms.Time.value += 0.01;
         Time += 0.01;
-        let variation = Math.sin(Time) * Math.sin(Time);
-        let posArr = [];
-        for (let i = 0; i < cube.geometry.attributes.position.count; i++) {
-            let xIndex = i * 3 + 0;
-            let yIndex = i * 3 + 1;
-            let zIndex = i * 3 + 2;
-            let vec = new Vector3(cube.geometry.attributes.position.array[xIndex], cube.geometry.attributes.position.array[yIndex], cube.geometry.attributes.position.array[zIndex]);
+        let variation = Math.sin(Time) * Math.sin(Time) * 1.5;
 
-            let p1 = new Vector3(0, 0, 0);
-            let r = planeIntersect(p1, vec, q, u, v);
-            //console.log(r);
-            let dist = 0;
-            if (0 <= r.x && r.x <= 1 && 0 <= r.y && r.y <= 1 && 0 <= r.z && r.z <= 1) {
-                dist = vec.length() * r.x;
-            }
-            //vec.setLength(1 * variation);
+        for (let i = 0; i < morph.geometry.attributes.position.count; i++) {
+            let v = new Vector3();
+            v.fromBufferAttribute(morph.geometry.attributes.position,i);
+
+            let dist = distToCube(morph.position,v);
+            
+            v.setLength(1 * sMax(dist, variation, 0.5));
             //vec.setLength(1 * Math.max(dist, variation));
-            vec.setLength(1 * sMax(dist, variation*1.5, 0.5));
-            posArr.push(vec.x);
-            posArr.push(vec.y);
-            posArr.push(vec.z);
-        }
-        let nArr = [];
-        for (let i = 0; i < posArr.length / 3; i++) {
-            let xIndexU = i * 3 + 0;
-            let yIndexU = i * 3 + 1;
-            let zIndexU = i * 3 + 2;
 
-            let U = new Vector3(posArr[xIndexU], posArr[yIndexU], posArr[zIndexU]);
-
-            U.normalize();
-            nArr.push(U.x);
-            nArr.push(U.y);
-            nArr.push(U.z);
+            morph.geometry.attributes.position.setXYZ(i,v.x,v.y,v.z);
         }
 
-        cube.geometry.attributes.position.set(posArr, 0);
-        cube.geometry.attributes.normal.set(nArr, 0);
-        cube.geometry.attributes.position.needsUpdate = true;
+        morph.geometry.computeVertexNormals();
+        morph.geometry.attributes.position.needsUpdate = true;
 
         renderer.render(scene, camera);
     }
